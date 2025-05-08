695084ec2ab74295cabddb328688201098235aa6
Kristijan Turić <kristijan.turic1@gmail.com>
2025-05-08 12:05:06 +0200

    Add inital frontend

---
+++ taskflow.client/src/App.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import api from "./services/api.js";
+import TaskList from "./components/TaskList";
+import AddTask from "./components/AddTask";
+import SearchTask from "./components/SearchTask.jsx";
+import './App.css';
+
+function App() {
+}
+
+export default App;
